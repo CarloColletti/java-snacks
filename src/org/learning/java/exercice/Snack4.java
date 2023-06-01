b34dfde4975a7a0c76d222593b2e0dcf5e0344a4
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-01 16:07:40 +0200

    snack4 not work

---
+++ src/org/learning/java/exercice/Snack4.java
@@ -0,0 +1,26 @@
+package org.learning.java.exercice;
+
+import java.util.Scanner;
+
+public class Snack4 {
+    //Data in ingresso una stringa verificare se è palindroma
+    public static void main(String[] args) {
+        //import scanner
+        Scanner wordIn = new Scanner(System.in);
+
+        //request word
+        String word = wordIn.next();
+
+
+        //boolean variable
+        boolean isPalindrome = word.equalsIgnoreCase(new StringBuilder(word).reverse().toString());
+
+        if (isPalindrome) {
+            //is Palindrome
+            System.out.println(word + " è palindroma.");
+        } else {
+            //isn't Palindrome
+            System.out.println(word + " non è palindroma.");
+        }
+    }
+}
