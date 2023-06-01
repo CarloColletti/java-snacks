c155151071c050ee7055c54a8a829f16f3d4e645
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-01 14:45:18 +0200

    snack 9

---
+++ src/org/learning/java/exercice/Snack9.java
@@ -0,0 +1,23 @@
+package org.learning.java.exercice;
+
+import java.util.Scanner;
+
+public class Snack9 {
+    //Calcola la somma e la media dei primi 10 numeri.
+    public static void main(String[] args){
+        //Variabili
+        int n = 10;
+        int sum = 0;
+
+        // Calcolo la somma dei primi 10 numeri
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        // Calcola la media dei primi 10 numeri
+        double media = (double) sum / n;
+
+        System.out.println("La somma dei primi 10 numeri è: " + sum);
+        System.out.println("La media dei primi 10 numeri è: " + media);
+    }
+}
